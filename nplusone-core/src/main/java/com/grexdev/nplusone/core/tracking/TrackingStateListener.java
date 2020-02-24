b548a4396971b140654fab8561d588a69dbba29b
Adam Gaj <adga@grexdev.com>
2020-02-24 22:20:35 +0100

    handling nested sessions

--- nplusone-core/src/main/java/com/grexdev/nplusone/core/tracking/TrackingStateListener.java
+++ nplusone-core/src/main/java/com/grexdev/nplusone/core/tracking/TrackingStateListener.java
@@ -5,6 +5,7 @@ import com.grexdev.nplusone.core.proxy.StateListener;
 import com.grexdev.nplusone.core.registry.RootNode;
 import com.grexdev.nplusone.core.registry.SessionNode;
 import com.grexdev.nplusone.core.registry.SessionToString;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,7 +19,7 @@ public class TrackingStateListener implements StateListener {
 
     private final FramesProvider framesProvider;
 
-    private final ThreadLocal<SessionNode> currentSession = new ThreadLocal<>();
+    private final ThreadLocal<SessionStack> currentSessionStack = new ThreadLocal<>();
 
     public TrackingStateListener(TrackingContext context, RootNode root) {
         this.root = root;
@@ -30,17 +31,28 @@ public class TrackingStateListener implements StateListener {
         log.info("########## SESSION CREATED ##########");
 
         SessionNode session = SessionNode.of(framesProvider.captureCallFrames());
-        root.addSession(session);
-        currentSession.set(session);
+        SessionStack sessionStack = currentSessionStack.get();
+
+        if (sessionStack != null) {
+            sessionStack.increaseSessionAmount();
+        } else {
+            root.addSession(session);
+            currentSessionStack.set(SessionStack.of(session));
+        }
     }
 
     @Override
     public void sessionClosed() {
         log.info("########## SESSION CLOSED ##########");
+        SessionStack sessionStack = currentSessionStack.get();
 
-        if (currentSession.get() != null) {
-            log.info(SessionToString.toString(currentSession.get()));
-            currentSession.remove();
+        if (sessionStack != null) {
+            log.info(SessionToString.toString(sessionStack.outerSessionNode));
+            sessionStack.decreaseSessionAmount();
+
+            if (sessionStack.allSessionsClosed()) {
+                currentSessionStack.remove();
+            }
 
         } else {
             log.warn("Session has been closed already");
@@ -51,9 +63,10 @@ public class TrackingStateListener implements StateListener {
     public void statementExecuted(String sql) {
         log.info("########## STATEMENT EXECUTED: {} ##########", sql);
 
-        SessionNode session = currentSession.get();
+        SessionStack sessionStack = currentSessionStack.get();
 
-        if (session != null) {
+        if (sessionStack != null) {
+            SessionNode session = sessionStack.outerSessionNode;
             session.addStatement(sql, framesProvider.captureCallFrames());
         } else {
             log.warn("Session has been closed already");
@@ -63,5 +76,29 @@ public class TrackingStateListener implements StateListener {
     @Override
     public void statementExecuted(Supplier<String> sqlSupplier) {
         statementExecuted(sqlSupplier.get());
+    }
+
+    @AllArgsConstructor
+    private static class SessionStack {
+
+        private final SessionNode outerSessionNode;
+
+        private int nestedSessionsAmount;
+
+        private static SessionStack of(SessionNode sessionNode) {
+            return new SessionStack(sessionNode, 1);
+        }
+
+        private void increaseSessionAmount() {
+            nestedSessionsAmount++;
+        }
+
+        private void decreaseSessionAmount() {
+            nestedSessionsAmount--;
+        }
+
+        private boolean allSessionsClosed() {
+            return nestedSessionsAmount == 0;
+        }
     }
 }
