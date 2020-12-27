74d914602799b64767e4abc45dc7239103a102ed
Adam Gaj <adgadev@gmail.com>
2020-12-27 13:59:47 +0100

    test: spring boot compatibility tests improved

--- jplusone-tests/jplusone-tests-compat-springboot20/src/test/java/com/adgadev/jplusone/test/BookshopControllerTest.java
+++ jplusone-tests/jplusone-tests-compat-springboot20/src/test/java/com/adgadev/jplusone/test/BookshopControllerTest.java
@@ -16,18 +16,28 @@
 
 package com.adgadev.jplusone.test;
 
+import com.adgadev.jplusone.core.report.ReportGenerator;
+import nl.altindag.log.LogCaptor;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -42,8 +52,13 @@ class BookshopControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @Test
     void shouldGetBookDetailsLazily() throws Exception {
+        LogCaptor reportLogCaptor = LogCaptor.forClass(ReportGenerator.class);
+
         mvc.perform(MockMvcRequestBuilders
                 .get("/book/lazy")
                 .accept(MediaType.APPLICATION_JSON))
@@ -51,10 +66,18 @@ class BookshopControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.title", equalTo("Godfather")))
                 .andExpect(jsonPath("$.author", equalTo("Mario Puzo")));
+
+
+        String expectedLog = loadTextFileFromClasspath("expected-reports/book-lazy-report.txt")
+                .replace("#LINE_NUMBER#", "62");
+        assertThat(reportLogCaptor.getDebugLogs(), hasSize(1));
+        assertThat(reportLogCaptor.getDebugLogs().get(0), equalTo(expectedLog));
     }
 
     @Test
     void shouldGetBookDetailsEagerly() throws Exception {
+        LogCaptor reportLogCaptor = LogCaptor.forClass(ReportGenerator.class);
+
         mvc.perform(MockMvcRequestBuilders
                 .get("/book/eager")
                 .accept(MediaType.APPLICATION_JSON))
@@ -62,6 +85,15 @@ class BookshopControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.title", equalTo("Godfather")))
                 .andExpect(jsonPath("$.author", equalTo("Mario Puzo")));
+
+        String expectedLog = loadTextFileFromClasspath("expected-reports/book-eager-report.txt")
+                .replace("#LINE_NUMBER#", "81");
+        assertThat(reportLogCaptor.getDebugLogs(), hasSize(1));
+        assertThat(reportLogCaptor.getDebugLogs().get(0), equalTo(expectedLog));
     }
 
+    private String loadTextFileFromClasspath(String path) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + path);
+        return IOUtils.toString(resource.getInputStream(), UTF_8);
+    }
 }
